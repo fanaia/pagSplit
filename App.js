83d4bb654328e89fff64f192ac10aa71b3d7843c
fabio <fabio@pdvseven.com.br>
2023-02-28 13:39:08 -0300

    teste modal

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import {AutenticacaoProvider} from './src/contexts/AutenticacaoContext';
 import AppNavigator from './src/navigation/AppNavigator';
+import RootStackScreen from './src/screens/RootStackScreen';
 
 export default function App() {
-    return (
-        <AutenticacaoProvider>
-            <AppNavigator />
-        </AutenticacaoProvider>
-    );
+    return <RootStackScreen />;
+    // return (
+    //     <AutenticacaoProvider>
+    //         <AppNavigator />
+    //     </AutenticacaoProvider>
+    // );
 }
